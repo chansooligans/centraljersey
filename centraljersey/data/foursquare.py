f097d814c60194d07bbd2d952c360ba94eaf4640
chansooligans <cs2737@nyu.edu>
2023-05-20 19:52:31 -0400

    add docstrings to foursquare api loader

--- centraljersey/data/foursquare.py
+++ centraljersey/data/foursquare.py
@@ -4,6 +4,7 @@ import time
 from dataclasses import dataclass
 from functools import cached_property
 from pathlib import Path
+from typing import List
 
 import geopandas as gpd
 import pandas as pd
@@ -15,9 +16,18 @@ from centraljersey import cache
 
 @dataclass
 class CensusCentroids:
-    # Load the census tract data for New Jersey
+    """
+    A class for handling census tract shapefiles for New Jersey.
+    """
+
     @cached_property
-    def longlats(self):
+    def longlats(self) -> List[str]:
+        """
+        Returns a list of centroid coordinates (latitude, longitude) for census tracts in New Jersey.
+
+        Returns:
+            List[str]: A list of centroid coordinates in the format "latitude,longitude".
+        """
         df = gpd.read_file("../data/tl_2018_34_tract/tl_2018_34_tract.shp")
         return [
             f"{y},{x}" for x, y in zip(df.geometry.centroid.x, df.geometry.centroid.y)
@@ -26,24 +36,49 @@ class CensusCentroids:
 
 @dataclass
 class FoursquareDownload(CensusCentroids):
+    """
+    A class for downloading Foursquare data based on census tract centroids.
+    """
+
     secrets: dict
     company: str = "wawa"
 
     @property
-    def company_id(self):
+    def company_id(self) -> dict:
+        """
+        Returns a dictionary mapping company names to their Foursquare IDs.
+
+        Returns:
+            dict: A dictionary mapping company names to their Foursquare IDs.
+        """
         return {
             "wawa": "7dbc6a56-2391-4a50-b479-8b469beacebc",
             "dunkin": "2cb519f8-883c-4263-860a-cd83325fbb97",
         }
 
     @property
-    def headers(self):
+    def headers(self) -> dict:
+        """
+        Returns the headers for the Foursquare API request.
+
+        Returns:
+            dict: The headers for the Foursquare API request.
+        """
         return {
             "Authorization": self.secrets["foursquare"]["api_key"],
             "accept": "application/json",
         }
 
-    def querystring(self, longlat):
+    def querystring(self, longlat: str) -> dict:
+        """
+        Constructs the query string parameters for the Foursquare API request.
+
+        Args:
+            longlat (str): The centroid coordinates in the format "latitude,longitude".
+
+        Returns:
+            dict: The query string parameters for the Foursquare API request.
+        """
         return {
             "chains": self.company_id[self.company],
             "ll": longlat,
@@ -52,20 +87,46 @@ class FoursquareDownload(CensusCentroids):
             "sort": "DISTANCE",
         }
 
-    def query(self, longlat):
+    def query(self, longlat: str) -> dict:
+        """
+        Sends a GET request to the Foursquare API and retrieves the response as JSON.
+
+        Args:
+            longlat (str): The centroid coordinates in the format "latitude,longitude".
+
+        Returns:
+            dict: The JSON response from the Foursquare API.
+        """
         url = "https://api.foursquare.com/v3/places/search"
         response = requests.request(
             "GET", url, headers=self.headers, params=self.querystring(longlat)
         )
         return response.json()
 
-    def fp_out(self, i):
+    def fp_out(self, i: int) -> Path:
+        """
+        Returns the file path for saving the Foursquare data.
+
+        Args:
+            i (int): The index of the data.
+
+        Returns:
+            Path: The file path for saving the Foursquare data.
+        """
         directory = Path(f"../data/{self.company}/")
         directory.mkdir(parents=True, exist_ok=True)
         return directory / f"{i}.json"
 
     def save(self):
-        for i, x in tqdm.tqdm(enumerate(self.longlats[:2])):
+        """
+        Saves the Foursquare data for each centroid coordinate.
+        This method queries the Foursquare API for each centroid coordinate and saves the response as JSON.
+        The data is saved in separate files named "{i}.json" in the corresponding company directory.
+
+        Returns:
+            None
+        """
+        for i, x in tqdm.tqdm(enumerate(self.longlats)):
             result = self.query(longlat=x)
             if Path(self.fp_out(i)).exists():
                 continue
